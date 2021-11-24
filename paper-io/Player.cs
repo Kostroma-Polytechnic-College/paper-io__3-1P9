0286920f22f57eed8fc1f07812585d7dae0049d4
Bill44-NotAlone <denyc9@mail.ru>
2021-11-24 18:27:25 +0300

    Функция появления и разнос по файлам

---
+++ paper-io/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace paper_io
+{
+    class Player
+    {
+        private int x; //где?
+        private int y; //где №2?
+        private bool life; //Сдох?
+        private Color colorOfPlayer; //Негр?
+        public Player(Coordinate сoordinate, bool Life, Color color)
+        {
+            x = сoordinate.X;
+            y = сoordinate.Y;
+            life = Life;
+            colorOfPlayer = color;
+        }
+        public Player(Coordinate coordinate)
+        {
+            x = coordinate.X;
+            y = coordinate.Y;
+        }
+    }
+}
