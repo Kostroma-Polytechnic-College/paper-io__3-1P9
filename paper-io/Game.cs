1763439f8100ef35d84ee85372e17d1faaec8ee4
Malena-AV <m.alena07082003@gmail.com>
2021-12-10 10:38:52 +0300

    исправлено

--- paper-io/Game.cs
+++ paper-io/Game.cs
@@ -2,13 +2,6 @@
 {
     class Game
     {
-        /// <summary>
-        /// Количество игроков
-        /// </summary>
-        public Player[,] Players { get; set; }
-        public Game(byte players)
-        {
-            Players = new Player[players * 10, players * 10];
-        }
+        
     }
 }
