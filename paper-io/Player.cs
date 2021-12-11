404e4a70861b166c7aad15ce430e0de9629f3706
andruha <molodoyelpil>
2021-12-11 08:47:07 +0300

    Начал работать над методом, увидел необходимость в перечислении Direction (в данной версии его нет)

--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -31,5 +31,57 @@ namespace paper_io
             Location = point;
             life = true;
         }
+
+        /// <summary>
+        /// Метод, отвечающий за изменение направления движения игрока/бота
+        /// </summary>
+        /// <param name="gamematrix"></param>
+        public void Bot(Player[,] players)
+        {
+            int x = (int) this.Location.X;
+            int y = (int) this.Location.Y;
+
+            /* Если со всех сторон находится территория текущего игрока, то направление 
+               движения не менять*/
+            if (players[x, y + 1] == this
+                && players[x - 1, y] == this
+                && players[x + 1, y] == this
+                && players[x, y - 1] == this
+                )
+            {
+                return;
+            }
+
+            /* Если впереди текущего игрока находится стена и слева нет стены, то повернуть налево. */
+            if (x == 0 && y > 0)
+            {
+                this.direction = Direction.Left;
+                return;
+            }
+
+            /*  Если впереди игрока находится стена и справа нет стены, то повернуть направо. */
+            if (this.x == 0 && this.y < players.Length - 1)
+            {
+                this.direction = Direction.Right;
+                return;
+            }
+
+            /* Если впереди и слева и справа нет территории текущего игрока, то повернуть
+               направо если там нет стены или повернуть налево если справа есть стена. Если впереди нет территории  */
+            if (players[x, y + 1] == this
+                && players[x - 1, y] == this
+                && players[x + 1, y] == this
+                )
+            {
+                if (this.x != 0 || this.y < players.Length - 1)
+                {
+                    this.direction = Direction.Right;
+                }
+                else
+                {
+                    this.direction = Direction.Left;
+                }
+            }
+        }
     }
 }
